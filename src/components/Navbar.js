d07e7dfd18cfa76a5255801b3d2002c2ed36f293
Noam Muchtar <noam@Mini-sl-Noam.lan>
2024-11-30 23:33:24 +0200

    inital commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+function Navbar() {
+    return ( 
+        <div>
+            <a href="/">Home</a>
+            <a href="/contact">Contact us</a>
+        </div>
+     );
+}
+
+export default Navbar;
